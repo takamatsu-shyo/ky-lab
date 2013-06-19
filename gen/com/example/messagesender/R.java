bbd166fea100565040842e389d34bc7fd9c849a8
syano <takamatsu.shyo@gmail.com>
2013-06-19 23:15:40 +0900

    put on kaeruImage

--- gen/com/example/messagesender/R.java
+++ gen/com/example/messagesender/R.java
@@ -22,22 +22,28 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int kaeru_mainview=0x7f020001;
     }
     public static final class id {
-        public static final int TextView01=0x7f080007;
-        public static final int action_settings=0x7f08000d;
-        public static final int adView=0x7f080003;
-        public static final int bnImportAddress=0x7f080004;
-        public static final int btnGmail=0x7f080002;
-        public static final int btnRegister=0x7f08000c;
-        public static final int btnSetting=0x7f080001;
-        public static final int editMessage=0x7f08000b;
-        public static final int editRecieverAdress=0x7f080006;
-        public static final int editSenderName=0x7f080008;
-        public static final int editTitle=0x7f080009;
-        public static final int textView1=0x7f080000;
-        public static final int textView2=0x7f080005;
-        public static final int textView3=0x7f08000a;
+        public static final int TextView01=0x7f08000c;
+        public static final int action_settings=0x7f080012;
+        public static final int adView=0x7f080000;
+        public static final int bnImportAddress=0x7f080009;
+        public static final int btnClose=0x7f080008;
+        public static final int btnGmail=0x7f080007;
+        public static final int btnRegister=0x7f080011;
+        public static final int btnSetting=0x7f080006;
+        public static final int editMessage=0x7f080010;
+        public static final int editRecieverAdress=0x7f08000b;
+        public static final int editSenderName=0x7f08000d;
+        public static final int editTitle=0x7f08000e;
+        public static final int frameLayout1=0x7f080001;
+        public static final int kaeruimage=0x7f080003;
+        public static final int linearLayout1=0x7f080002;
+        public static final int linearLayout2=0x7f080004;
+        public static final int textView1=0x7f080005;
+        public static final int textView2=0x7f08000a;
+        public static final int textView3=0x7f08000f;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
